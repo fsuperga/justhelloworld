427c5ed9fac1ee654f76cab6c8ca7012cd5005f5
fsuperga <fsuperga@gmail.com>
2012-07-22 16:21:04 +0200

    a

--- index.php
+++ index.php
@@ -94,20 +94,11 @@ if ($user) {
               $param = '/me/mutualfriends/'.$friendList['id'];
               $mutual_friends     =   $facebook->api($param);
 
-              echo "<tr><td colspan = '2' bgcolor = '#111111'><img src='https://graph.facebook.com/".$friendList['id']."/picture' width='50' height='50' title='".$friendList['name']."' />".$friendList['name']."</td>";
+              echo "<tr><td colspan = '2' bgcolor = '#CCFFFF'><img src='https://graph.facebook.com/".$friendList['id']."/picture' width='50' height='50' title='".$friendList['name']."' />".$friendList['name']."</td></tr>";
 
               foreach ($mutual_friends['data'] as $key2=>$mutualFriendList)
               {
-                  if ($key2 ==1)
-                  {
-                      echo "<td><img src='https://graph.facebook.com/".$mutualFriendList['id']."/picture' width='50' height='50' title='".$mutualFriendList['name']."' />".$mutualFriendList['name']."</td></tr>";
-                  }
-                  else
-                  {
-                      echo "<tr><td></td><td><img src='https://graph.facebook.com/".$mutualFriendList['id']."/picture' width='50' height='50' title='".$mutualFriendList['name']."' />".$mutualFriendList['name']."</td></tr>";
-
-                  }
-
+                  echo "<tr><td></td><td>".$key2."<img src='https://graph.facebook.com/".$mutualFriendList['id']."/picture' width='50' height='50' title='".$mutualFriendList['name']."' />".$mutualFriendList['name']."</td></tr>";
               }
           }
         ?>
