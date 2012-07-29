be456cc1b39ea7252d1c14ed5a370f8f63f2e889
fsuperga <fsuperga@gmail.com>
2012-07-29 14:15:24 +0200

    a

--- src/photostack.php
+++ src/photostack.php
@@ -1,16 +1,8 @@
 <?php
-//$location 	= 'albums';
-$userid         = $_GET['userid'];
-$param          = '/me/mutualfriends/'.$userid ;
-$mutual_friends = $facebook->api($param);
-$images         = array();
-foreach ($mutual_friends['data'] as $key=>$mutualFriendList)
-{
-    $images[] = "https://graph.facebook.com/".$mutualFriendList['id']."/picture";
-}
-//$files 		= glob($location . '/' . $album_name . '/*.{jpg,gif,png}', GLOB_BRACE);
-//$encoded 	= json_encode($files);
-$encoded 	= json_encode($images);
-
+$location 	= 'albums';
+$album_name	= $_GET['album_name'];
+$files 		= glob($location . '/' . $album_name . '/*.{jpg,gif,png}', GLOB_BRACE);
+$encoded 	= json_encode($files);
 echo $encoded;
 unset($encoded);
+?>
---
+++ src/photostack2.php
@@ -0,0 +1,16 @@
+<?php
+//$location 	= 'albums';
+$userid         = $_GET['userid'];
+$param          = '/me/mutualfriends/'.$userid ;
+$mutual_friends = $facebook->api($param);
+$images         = array();
+foreach ($mutual_friends['data'] as $key=>$mutualFriendList)
+{
+    $images[] = "https://graph.facebook.com/".$mutualFriendList['id']."/picture";
+}
+//$files 		= glob($location . '/' . $album_name . '/*.{jpg,gif,png}', GLOB_BRACE);
+//$encoded 	= json_encode($files);
+$encoded 	= json_encode($images);
+
+echo $encoded;
+unset($encoded);
